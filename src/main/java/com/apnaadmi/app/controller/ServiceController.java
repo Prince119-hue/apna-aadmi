f983a2fc28601b3970527fc3169fd402b772f005
Prince <pm7918263@gmail.com>
2026-02-28 18:08:30 +0530

    Fixed service page routing

--- src/main/java/com/apnaadmi/app/controller/ServiceController.java
+++ src/main/java/com/apnaadmi/app/controller/ServiceController.java
@@ -1,16 +1,9 @@
 package com.apnaadmi.app.controller;
 
-import com.apnaadmi.app.entity.ServiceRequest;
-import com.apnaadmi.app.entity.User;
-import com.apnaadmi.app.repository.ServiceRequestRepository;
-import com.apnaadmi.app.repository.UserRepository;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
-@Profile("db")
 @Controller
 @RequestMapping("/services")
 public class ServiceController {
