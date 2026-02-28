0edb0bc56070b7a5d0dfdb2a9419773e6dd48359
Prince <pm7918263@gmail.com>
2026-02-28 15:26:11 +0530

    Disabled DB autoconfig in main class

--- src/main/java/com/apnaadmi/app/ApnaAadmiApplication.java
+++ src/main/java/com/apnaadmi/app/ApnaAadmiApplication.java
@@ -2,15 +2,12 @@ package com.apnaadmi.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
 @SpringBootApplication(
         exclude = {
-                DataSourceAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class,
-                JpaRepositoriesAutoConfiguration.class
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class
         }
 )
 public class ApnaAadmiApplication {
