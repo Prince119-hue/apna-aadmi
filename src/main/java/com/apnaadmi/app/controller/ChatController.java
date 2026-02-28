0f2fb93dd06598a1f07482d74526e790199874a1
Prince <pm7918263@gmail.com>
2026-02-28 14:56:58 +0530

    Disabled all DB dependent controllers

--- src/main/java/com/apnaadmi/app/controller/ChatController.java
+++ src/main/java/com/apnaadmi/app/controller/ChatController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@RestController
+//@RestController
 @RequestMapping("/api/chat")
 @CrossOrigin("*")
 public class ChatController {
